68e66a5e5529f190b5675bf95793ffd74f81084f
Andy Yang <awy6143@gmail.com>
2020-07-19 22:28:16 -0500

    simple form to accept commander slug

--- src/main/kotlin/elder/dragon/hipster/edhcalc/controller/DeckController.kt
+++ src/main/kotlin/elder/dragon/hipster/edhcalc/controller/DeckController.kt
@@ -1,25 +1,34 @@
 package elder.dragon.hipster.edhcalc.controller
 
-import com.github.kittinunf.fuel.gson.responseObject
 import com.github.kittinunf.fuel.httpGet
 import elder.dragon.hipster.edhcalc.model.EdhrecDeck
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 public class DeckController {
 
-    @GetMapping("/")
-    fun render(model: Model): String {
-        val asyncHttp = "https://edhrec-json.s3.amazonaws.com/en/decks/nazahn-revered-bladesmith.json"
+    @GetMapping
+    fun render(model: Model) : String {
+        model["deck"] = ""
+        return "page"
+    }
+
+    @PostMapping
+    fun process(model: Model, @RequestParam slug: String): String {
+        val asyncHttp = "https://edhrec-json.s3.amazonaws.com/en/decks/$slug.json"
                 .httpGet()
                 .responseObject(EdhrecDeck.Deserializer()) { _, _, result ->
                     val (deck, error) = result
 
                     if (deck != null) {
                         model["deck"] = deck.decklist
+                    } else {
+                        model["deck"] = "Not found"
                     }
                 }
 
